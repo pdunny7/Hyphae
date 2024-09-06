ae67aaa9ac4c4099fbeff6939755a8599bcfb633
pdunny7 <88473135+pdunny7@users.noreply.github.com>
2024-09-06 12:22:33 -0400

    Create routes.py and auth folder

---
+++ app/auth/routes.py
@@ -0,0 +1,10 @@
+# app/auth/routes.py
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from app.models.user import User
+from app.database import get_db
+from config import settings
