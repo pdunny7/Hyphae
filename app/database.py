9709dcf843acf894f054e88acf41ab57fc66028e
pdunny7 <88473135+pdunny7@users.noreply.github.com>
2024-09-06 17:48:53 +0000

    Add get_db function

--- app/database.py
+++ app/database.py
@@ -10,3 +10,11 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Add the get_db function
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
